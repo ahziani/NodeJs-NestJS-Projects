003bdb526c697a0a3176ce9c76f1aeba27791598
ahmed <ahziani@sqli.com>
2024-09-09 17:04:22 +0100

    EJS

--- 2 - Express js/app.js
+++ 2 - Express js/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const userRoutes = require('./routes/user');
 const adminRoutes = require('./routes/admin');
@@ -8,5 +12,8 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(adminRoutes)
 app.use(userRoutes)
+app.use((req, res) => {
+    res.status(404).render('404');
+})
 
 app.listen(3000);
--- 2 - Express js/routes/admin.js
+++ 2 - Express js/routes/admin.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const path = require('path');
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/post-product" method="POST">'+
-        'Title : <input type="text" name="title"/>'+
-        '<button>Add</button>'+
-    '</form>')
+    res.sendFile(path.join(__dirname,'../views/add-product.html'))
 })
 
 router.post('/post-product', (req, res, next) => {
